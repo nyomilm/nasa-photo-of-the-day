c666bd05c544b32f6254fac8cb75f4773abccee1
nyomilm <nyomilm@hotmail.com>
2019-08-01 21:45:00 -0500

    3rd update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
+
+// import PhotoCard from "./Components/PhotoCard";
+import PhotoList from "./Components/PhotoList";
+import PhotoWatch from "./Components/PhotoWatch";
+
+
 
 function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState("react");
+
+  const [photo, setPhoto] = useState([]);
+  useEffect(() =>{
+    const fetchData = () => {
+    axios
+    .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+
+    .then (result => setPhoto(result.data));
+  };
+  
+  fetchData();
+}, );
+
   return (
     <div className="App">
+      <h1>NASA PHOTO OF THE DAY</h1>
+      {/* <PhotoList /> */} 
+      {/* <PhotoWatch /> */}
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
